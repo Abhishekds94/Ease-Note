1f97dabcc5e53c336224b65581a8655e760cdc94
Abhishek D S <abhishekds94@gmail.com>
2020-06-21 01:03:38 -0500

    Added splash screen and home screen

---
+++ app/src/main/java/com/abhishek/easenote/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.abhishek.easenote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/abhishek/easenote/MainActivity.kt
+++ app/src/main/java/com/abhishek/easenote/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.abhishek.easenote
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
+    // Loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Handler().postDelayed({
+            startActivity(Intent(this,HomeActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
