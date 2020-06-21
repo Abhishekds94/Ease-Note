05da1a005aca8ef9fc4f13e3a812a519a3fd03e5
Abhishek D S <abhishekds94@gmail.com>
2020-06-21 15:09:40 -0500

    Added listeners in home page

--- app/src/main/java/com/abhishek/easenote/HomeActivity.kt
+++ app/src/main/java/com/abhishek/easenote/HomeActivity.kt
@@ -1,11 +1,29 @@
 package com.abhishek.easenote
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var addNotes: FloatingActionButton
+    private lateinit var notesRV: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //View init
+        addNotes = findViewById(R.id.addNotes)
+        notesRV = findViewById(R.id.rv_notes)
+
+        //AddButon onClick
+        addNotes.setOnClickListener() {
+            startActivity(Intent(this,AddNotesActivity::class.java))
+            finish()
+        }
+
     }
 }
