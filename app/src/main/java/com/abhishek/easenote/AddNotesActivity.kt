d5efd6c1bec0c649c0fdcbe461086aebdda989d6
Abhishek D S <abhishekds94@gmail.com>
2020-06-21 15:17:37 -0500

    Add notes UI

--- app/src/main/java/com/abhishek/easenote/AddNotesActivity.kt
+++ app/src/main/java/com/abhishek/easenote/AddNotesActivity.kt
@@ -8,5 +8,6 @@ class AddNotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+
     }
 }
