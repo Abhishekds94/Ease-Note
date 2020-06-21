8e23e32a9d20e4736244b92f334bbee842f3f961
Abhishek D S <abhishekds94@gmail.com>
2020-06-21 16:45:55 -0500

    Added connection btwn DB and Views

--- app/src/main/java/com/abhishek/easenote/HomeActivity.kt
+++ app/src/main/java/com/abhishek/easenote/HomeActivity.kt
@@ -3,13 +3,19 @@ package com.abhishek.easenote
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import io.realm.Realm
+import io.realm.RealmResults
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var addNotes: FloatingActionButton
     private lateinit var notesRV: RecyclerView
+    private lateinit var notesList: ArrayList<Notes>
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,7 @@ class HomeActivity : AppCompatActivity() {
         //View init
         addNotes = findViewById(R.id.addNotes)
         notesRV = findViewById(R.id.rv_notes)
+        realm = Realm.getDefaultInstance()
 
         //AddButon onClick
         addNotes.setOnClickListener() {
@@ -25,5 +32,15 @@ class HomeActivity : AppCompatActivity() {
             finish()
         }
 
+        notesRV.layoutManager = StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL)
+        getAllNotes()
+
+    }
+
+    private fun getAllNotes(){
+        notesList = ArrayList()
+        var results:RealmResults<Notes> = realm.where<Notes>(Notes::class.java).findAll()
+        notesRV.adapter = NotesAdapter(this, results)
+        notesRV.adapter!!.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/abhishek/easenote/NotesAdapter.kt
+++ app/src/main/java/com/abhishek/easenote/NotesAdapter.kt
@@ -2,17 +2,20 @@ package com.abhishek.easenote
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import io.realm.RealmList
+import io.realm.RealmResults
+import kotlinx.android.synthetic.main.rv_notes_layout.view.*
 
-class NotesAdapter (
-    private val context: Context?, private val notesList: RealmList<Notes>
+class NotesAdapter(
+    private val context: Context?, private val notesList: RealmResults<Notes>
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.rv_notes_layout, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.rv_notes_layout, parent, false)
 
-        return RecyclerView.ViewHolder(v)
+        return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
@@ -20,7 +23,15 @@ class NotesAdapter (
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.itemView.tv_title.text = notesList[position]!!.title
+        holder.itemView.tv_desc.text = notesList[position]!!.desc
+        holder.itemView.tv_id.text = notesList[position]!!.id.toString()
+    }
+
+    class ViewHolder(v: View?): RecyclerView.ViewHolder(v!!){
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val desc = itemView.findViewById<TextView>(R.id.tv_desc)
+        val id = itemView.findViewById<TextView>(R.id.tv_id)
     }
 
 }
